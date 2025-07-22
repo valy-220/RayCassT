e89105d3111ba5c59a60451408884499f144e750
valy-220 <vstoica@yahoo.com>
2025-07-22 03:04:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,44 +1,60 @@
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
+from PIL import Image, ImageOps, ImageDraw
+from sklearn.decomposition import PCA
+from numba import jit
 import sys
 import math
 import numpy as np
+import time
 
-# parametri inițiali
-R = 400.0
-D = 200.0
-d_gura = 50.0
-R2 = 160.0
-D2 = 50.0
-dDD = 145.0
-Bf = -20.0
-plane = 35.0
 
-l_triunghi = 11.03
-l_triunghi2 = 2.53
+# parametrii inițiali
+
+# oglinzi 1, 2
 concava = True
+R = 376.0
+D = 200.0
+d_gura = 60.0
+
 concava2 = True
+R2 = 129.0
+D2 = 50.0
+
+dDD = 139.0
+
+Bf = -20.0
+
+# senzor (in mm)
+plane = 35.0
+pixel_size = 0.1
+
+# diferente finite
+l_triunghi = 3.0
+l_triunghi2 = 2
+
 N_THETA = 40
 M_PHI = 80
 
 # control vizualizare
 zoom = 30.0
-rotate_x = 0.0
-rotate_y = 0.0
+rotate_x, rotate_y = 0.0, 0.0
 last_x, last_y = 0, 0
 mouse_down = False
 
-
+@jit(nopython=True)
 def normalize(v):
     return v / np.linalg.norm(v)
 
+@jit(nopython=True)
 def compute_normal(A, B, C):
     AB = B - A
     AC = C - A
     N = np.cross(AB, AC)
     return normalize(N)
 
+@jit(nopython=True)
 def compute_triangle_center(v0, v1, v2):
     v0, v1, v2 = np.array(v0), np.array(v1), np.array(v2)
     # Centru geometric
@@ -83,6 +99,7 @@ def generate_spherical_cap_triangles(R, D, N_theta, M_phi, concave=True, d=0.0):
                   sign * (R - R * math.cos(theta1)))
 
             # funcție ajutătoare — verifică dacă punctul e în afara găurii
+            @jit(nopython=True)
             def is_outside_hole(p):
                 r = math.hypot(p[0], p[1])
                 return r >= r_min
@@ -271,6 +288,7 @@ def generate_uniform_triangular_mesh(R, D, d=0.0, concave=True, edge_length=0.5)
 
 def generate_init_rays(triangles):
     rays = []
+    intermediareRays = []
     
     for tri in triangles:
         p1 = (np.array(tri[0]) + np.array(tri[1]) + np.array(tri[2])) / 3.0
@@ -278,8 +296,30 @@ def generate_init_rays(triangles):
         p2 = (np.array(tri[0]) + np.array(tri[1]) + np.array(tri[2])) / 3.0
         p2[2] = 999999.99
         rays.append((p1, p2))
-             
-    return rays
+        ray_out = reflect_ray_to_plane(p1, p2, tri, -1.5 * dDD)
+        if ray_out is not None:
+            intermediareRays.append(ray_out)
+                
+    # return rays[:200]         
+    return intermediareRays
+
+@jit(nopython=True)
+def point_in_triangle(P, A, B, C):
+    v0 = C - A
+    v1 = B - A
+    v2 = P - A
+
+    dot00 = np.dot(v0, v0)
+    dot01 = np.dot(v0, v1)
+    dot02 = np.dot(v0, v2)
+    dot11 = np.dot(v1, v1)
+    dot12 = np.dot(v1, v2)
+
+    invDenom = 1 / (dot00 * dot11 - dot01 * dot01)
+    u = (dot11 * dot02 - dot01 * dot12) * invDenom
+    v = (dot00 * dot12 - dot01 * dot02) * invDenom
+
+    return (u >= 0) and (v >= 0) and (u + v <= 1)
 
 def reflect_ray_to_plane(ray_start, ray_end, triangle_vertices, z_target):
     A, B, C = [np.array(v) for v in triangle_vertices]
@@ -303,22 +343,22 @@ def reflect_ray_to_plane(ray_start, ray_end, triangle_vertices, z_target):
 
     P = P0 + t * D
 
-    def point_in_triangle(P, A, B, C):
-        v0 = C - A
-        v1 = B - A
-        v2 = P - A
+    # def point_in_triangle(P, A, B, C):
+    #     v0 = C - A
+    #     v1 = B - A
+    #     v2 = P - A
 
-        dot00 = np.dot(v0, v0)
-        dot01 = np.dot(v0, v1)
-        dot02 = np.dot(v0, v2)
-        dot11 = np.dot(v1, v1)
-        dot12 = np.dot(v1, v2)
+    #     dot00 = np.dot(v0, v0)
+    #     dot01 = np.dot(v0, v1)
+    #     dot02 = np.dot(v0, v2)
+    #     dot11 = np.dot(v1, v1)
+    #     dot12 = np.dot(v1, v2)
 
-        invDenom = 1 / (dot00 * dot11 - dot01 * dot01)
-        u = (dot11 * dot02 - dot01 * dot12) * invDenom
-        v = (dot00 * dot12 - dot01 * dot02) * invDenom
+    #     invDenom = 1 / (dot00 * dot11 - dot01 * dot01)
+    #     u = (dot11 * dot02 - dot01 * dot12) * invDenom
+    #     v = (dot00 * dot12 - dot01 * dot02) * invDenom
 
-        return (u >= 0) and (v >= 0) and (u + v <= 1)
+    #     return (u >= 0) and (v >= 0) and (u + v <= 1)
 
     if not point_in_triangle(P, A, B, C):
         return None
@@ -362,22 +402,22 @@ def reflect_ray_to_plane2(ray_start, ray_end, triangle_vertices, z_target):
 
     P = P0 + t * D
 
-    def point_in_triangle(P, A, B, C):
-        v0 = C - A
-        v1 = B - A
-        v2 = P - A
+    # def point_in_triangle(P, A, B, C):
+    #     v0 = C - A
+    #     v1 = B - A
+    #     v2 = P - A
 
-        dot00 = np.dot(v0, v0)
-        dot01 = np.dot(v0, v1)
-        dot02 = np.dot(v0, v2)
-        dot11 = np.dot(v1, v1)
-        dot12 = np.dot(v1, v2)
+    #     dot00 = np.dot(v0, v0)
+    #     dot01 = np.dot(v0, v1)
+    #     dot02 = np.dot(v0, v2)
+    #     dot11 = np.dot(v1, v1)
+    #     dot12 = np.dot(v1, v2)
 
-        invDenom = 1 / (dot00 * dot11 - dot01 * dot01)
-        u = (dot11 * dot02 - dot01 * dot12) * invDenom
-        v = (dot00 * dot12 - dot01 * dot02) * invDenom
+    #     invDenom = 1 / (dot00 * dot11 - dot01 * dot01)
+    #     u = (dot11 * dot02 - dot01 * dot12) * invDenom
+    #     v = (dot00 * dot12 - dot01 * dot02) * invDenom
 
-        return (u >= 0) and (v >= 0) and (u + v <= 1)
+    #     return (u >= 0) and (v >= 0) and (u + v <= 1)
 
     if not point_in_triangle(P, A, B, C):
         return None
@@ -398,20 +438,32 @@ def reflect_ray_to_plane2(ray_start, ray_end, triangle_vertices, z_target):
 
 def reflect_rays_to_plane(rays, triangles, z_target):
     rays_out = []
-    for tri in triangles:
-        for line in rays:
+    # for tri in triangles:
+    #     for line in rays:
+    #         ray_out = reflect_ray_to_plane(line[0], line[1], tri, -1.5 * dDD)
+    #         if ray_out is not None:
+    #             rays_out.append(ray_out)
+    for line in rays:
+        for tri in triangles:
             ray_out = reflect_ray_to_plane(line[0], line[1], tri, -1.5 * dDD)
             if ray_out is not None:
                 rays_out.append(ray_out)
+                break
     return rays_out
 
 def reflect_rays_to_plane2(rays, triangles, z_target):
     rays_out = []
-    for tri in triangles:
-        for line in rays:
+    # for tri in triangles:
+    #     for line in rays:
+    #         ray_out = reflect_ray_to_plane2(line[0], line[1], tri, -1.0 * Bf)
+    #         if ray_out is not None:
+    #             rays_out.append(ray_out)
+    for line in rays:
+        for tri in triangles:
             ray_out = reflect_ray_to_plane2(line[0], line[1], tri, -1.0 * Bf)
             if ray_out is not None:
                 rays_out.append(ray_out)
+                break
     return rays_out
 
 def draw_wireframe_surface(triangles):
@@ -425,13 +477,43 @@ def draw_wireframe_surface(triangles):
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
 
 def draw_plane(size):
+    
+   
+    
+    #img = img.transpose(Image.FLIP_TOP_BOTTOM)
+    img_data = img.tobytes()
+
+    texture_id = glGenTextures(1)
+    glEnable(GL_TEXTURE_2D)
+    glBindTexture(GL_TEXTURE_2D, texture_id)
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img.width, img.height, 0,
+                 GL_RGB, GL_UNSIGNED_BYTE, img_data)
+    
+
+
+
     size = size / 2.0
-    glBegin(GL_LINE_LOOP)
+    glBegin(GL_QUADS)
+    glTexCoord2f(0.0, 0.0)
     glVertex3f(-size, -size, 0)
+    
+    glTexCoord2f(1.0, 0.0)
     glVertex3f( size, -size, 0)
+    
+    glTexCoord2f(1.0, 1.0)
     glVertex3f( size,  size, 0)
+    
+    glTexCoord2f(0.0, 1.0)
     glVertex3f(-size,  size, 0)
     glEnd()
+    
+    glDisable(GL_TEXTURE_2D)
 
 def draw_source_rays(rays):
     glEnable(GL_BLEND)
@@ -450,7 +532,7 @@ def draw_source_rays(rays):
 def draw_intermediate_rays(rays):
     glEnable(GL_BLEND)
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-    glColor4f(0.75, 0.75, 0.75, 0.45)  # gri cu transparență
+    glColor4f(0.75, 0.75, 0.75, 0.15)  # gri cu transparență
     
     glBegin(GL_LINES)
     for line in rays:
@@ -464,7 +546,7 @@ def draw_intermediate_rays(rays):
 def draw_final_rays(rays):
     glEnable(GL_BLEND)
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-    glColor4f(0.95, 0.95, 0.95, 0.65)  # gri cu transparență
+    glColor4f(0.95, 0.95, 0.95, 0.35)  # gri cu transparență
     
     glBegin(GL_LINES)
     for line in rays:
@@ -500,8 +582,7 @@ def display():
     
     glScalef(0.3, 0.3, 0.3)
     
-    
-    
+
     draw_plane(plane);
     
     glTranslatef(0.0, 0.0, Bf)
@@ -509,10 +590,10 @@ def display():
     glPushMatrix()
     glRotatef(180, 1.0, 0.0, 0.0)
     glTranslatef(0.0, 0.0, -1.0 * Bf)
-    draw_source_rays(source_rays)
+    #draw_source_rays(source_rays)
     glPopMatrix()
     
-    draw_intermediate_rays(intermediate_rays)
+    #draw_intermediate_rays(intermediate_rays)
     draw_final_rays(final_rays)
     
     glColor3f(0.7, 0.7, 0.9)
@@ -521,6 +602,7 @@ def display():
     glTranslatef(0.0, 0.0, -1 * dDD)
     draw_wireframe_surface(surface_triangles2)
     
+       
 
     glutSwapBuffers()
 
@@ -610,13 +692,25 @@ def motion(x, y):
 def main():
     global surface_triangles, surface_triangles2
     global source_rays, intermediate_rays, final_rays
+    global final_ponits
+    global img
 
     glutInit(sys.argv)
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+    #glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_MULTISAMPLE)
     glutInitWindowSize(900, 700)
     glutCreateWindow(b"RayCassT - Simulator telescop")
 
     glEnable(GL_DEPTH_TEST)
+    glEnable(GL_MULTISAMPLE)
+    glEnable(GL_LINE_SMOOTH)
+    #glEnable(GL_POINT_SMOOTH)
+    glHint(GL_LINE_SMOOTH_HINT, GL_NICEST)
+    #glHint(GL_POINT_SMOOTH_HINT, GL_NICEST)
+    
+    glEnable(GL_BLEND)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+
     glClearColor(0.1, 0.1, 0.15, 1.0)
 
     glutDisplayFunc(display)
@@ -625,23 +719,125 @@ def main():
     glutMouseFunc(mouse)
     glutMotionFunc(motion)
 
+
+    #START scenariu simulare
+
+    PARAM = "R2"
+    #with open("analiza_puncte.txt", "a") as f:
+    #    f.write("Parametru simulat\tValoare parametru\tDispersie\tExcentricitate\tElongatie\n")
+    
+    # for R2 in np.arange(129.0, 129.0, 1.0):
+    #     print(f"Parametru: {PARAM} - {R2:.3f})")
+                 
     # surface_triangles = generate_spherical_cap_triangles(R, D, N_THETA, M_PHI, concava, d=d_gura)
     # surface_triangles = generate_spherical_cap_triangles_uniform(R, D, N_THETA, M_PHI, concava, d=d_gura)
     # surface_triangles = generate_triangular_grid(R, D, d=d_gura, concave=concava, edge_length=0.1)
+    
+    start = time.time()
     surface_triangles = generate_uniform_triangular_mesh(R, D, d=d_gura, concave=concava, edge_length=l_triunghi)
+    end = time.time()
+    durata = end - start
+    print(f"Număr triunghiuri oglinda 1: {len(surface_triangles)} (in {durata:.2f}s)")
+    
+    start = time.time()
     surface_triangles2 = generate_uniform_triangular_mesh(R2, D2, d=0, concave=concava2, edge_length=l_triunghi2)
-    source_rays = generate_init_rays(surface_triangles)
-    intermediate_rays = reflect_rays_to_plane (source_rays, surface_triangles, dDD)
+    end = time.time()
+    durata = end - start
+    print(f"Număr triunghiuri oglinda 2: {len(surface_triangles2)} (in {durata:.2f}s)")
+    
+    start = time.time()
+    #source_rays = generate_init_rays(surface_triangles)
+    intermediate_rays = generate_init_rays(surface_triangles)
+    end = time.time()
+    durata = end - start
+    # print(f"Număr raze initiale: {len(source_rays)} (in {durata:.2f}s)")
+    print(f"Număr raze initiale: {len(intermediate_rays)} (in {durata:.2f}s)")
+    
+    # start = time.time()
+    # intermediate_rays = reflect_rays_to_plane (source_rays, surface_triangles, dDD)
+    # end = time.time()
+    # durata = end - start
+    print(f"Număr raze reflectate 1: {len(intermediate_rays)} (in {durata:.2f}s)")
+    
+    start = time.time()
     final_rays = reflect_rays_to_plane2 (intermediate_rays, surface_triangles2, dDD)
+    end = time.time()
+    durata = end - start
+    print(f"Număr raze reflectate 2: {len(final_rays)} (in {durata:.2f}s)")
     
-    
-    
-    print(f"Număr triunghiuri oglinda 1: {len(surface_triangles)}")
-    print(f"Număr triunghiuri oglinda 2: {len(surface_triangles2)}")
-    print(f"Număr raze initiale: {len(source_rays)}")
-    print(f"Număr raze reflectate 1: {len(intermediate_rays)}")
-    print(f"Număr raze reflectate 2: {len(final_rays)}")
+    start = time.time()
+    img_temp = Image.new('RGB', (int(plane/pixel_size) + 1, int(plane/pixel_size) + 1), color=(0, 0, 0))
 
+    # intersectii = 0
+    final_ponits = []
+    for line in final_rays:
+        p = line[1]
+        px = p[0]
+        py = p[1]
+        if px > -1 * plane / 2.0 and px < plane / 2.0 and py > -1 * plane / 2.0 and py < plane / 2.0:
+            color = img_temp.getpixel((int((px + plane / 2.0) / pixel_size), int((py + plane / 2.0) / pixel_size)))[0]
+            if color < 256:
+                color += 1;
+            img_temp.putpixel((int((px + plane / 2.0) / pixel_size), int((py + plane / 2.0) / pixel_size)), (color, color, color))
+            final_ponits.append(p)
+            # intersectii += 1
+
+
+    # 1. Centroid
+    centroid = np.mean(final_ponits, axis=0)
+    
+    # 2. Dispersie (media pătratică a distanțelor față de centroid)
+    distances = np.linalg.norm(final_ponits - centroid, axis=1)
+    mean_squared_distance = np.mean(distances**2)
+    
+    # 3. PCA pentru excentricitate și elongare
+    pca = PCA(n_components=2)
+    pca.fit(final_ponits)
+    explained_variance = pca.explained_variance_
+    
+    # Excentricitate = sqrt(1 - (b^2 / a^2)) pentru elipsa echivalentă
+    a2, b2 = explained_variance
+    excentricity = np.sqrt(1 - b2 / a2)
+    elongation = np.sqrt(a2 / b2)
+    
+    axis_lengths = 2 * np.sqrt(pca.explained_variance_)  # diametrul axelor
+    axis_major = axis_lengths[0]
+    axis_minor = axis_lengths[1]
+
+    # Afișare rezultate
+    print(f"Centroid: {centroid}")
+    print(f"Dispersie (media pătratică a distanțelor): {mean_squared_distance:.3f}")
+    print(f"Excentricitate: {excentricity:.3f} (0-cerc, 1-alungit)")
+    print(f"Axa mare / axa mică): {axis_major:.1f} / {axis_minor:.1f}")
+    print(f"Elongatie (axa mare / axa mică): {elongation:.3f}")
+
+    # points = np.array([...])  # punctele tale 2D, n x 2
+    points = np.array(final_ponits)
+    min_x = np.min(points[:, 0])
+    max_x = np.max(points[:, 0])
+    min_y = np.min(points[:, 1])
+    max_y = np.max(points[:, 1])
+    
+    # Bounding box = colțul stânga-jos + dimensiuni
+    width = max_x - min_x
+    height = max_y - min_y
+    print(f"Bounding box-ul): {width:.1f} x {height:.1f}")
+
+    #with open("analiza_puncte.txt", "a") as f:
+    #    #f.write("Param\tParam value\tDispersie\tExcentricitate\tElongare\n")
+    #    f.write(f"{PARAM}\t{R2:.3f}\t{mean_squared_distance:.3f}\t{excentricity:.3f}\t{elongation:.3f}\n")
+    
+    draw = ImageDraw.Draw(img_temp)
+    draw.rectangle([int((min_x + plane / 2.0) / pixel_size), int((min_y + plane / 2.0) / pixel_size),  int((max_x + plane / 2.0) / pixel_size), int((max_y + plane / 2.0) / pixel_size)], outline="red", width=1)
+    
+    filename = f"spot {PARAM} {R2:.3f}.png"
+    ImageOps.autocontrast(img_temp).save(filename)
+    img = ImageOps.autocontrast(img_temp.resize((256, 256), Image.LANCZOS))
+    # img.save("spot.png")
+    end = time.time()
+    durata = end - start
+    print(f"Număr raze  pe  senzor: {len(final_ponits)} (in {durata:.2f}s)")
+    
     glutMainLoop()
 
 if __name__ == "__main__":
